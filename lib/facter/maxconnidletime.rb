bde17b8ea8dbab53d999c7d0320f43cff67e8706
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-08-20 23:40:16 +0200

    rubocop fixes

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -5,9 +5,10 @@ Facter.add('maxconnidletime') do
   setcode do
     if Facter.value(:windows_server_type) == 'windowsdc'
       dcs = Facter::Core::Execution.execute('dsquery server')
-      dc_dn = dcs.lines.first.gsub!('"', '').split(',')
+      dc_dn = dcs.lines.first.delete('"').split(',')
       domain_dn = dc_dn.last(2).join(',').chomp
-      time = Facter::Core::Execution.execute("dsquery * \"CN=Default Query Policy,CN=Query-Policies,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,#{domain_dn}\" -attr LDAPAdminLimits")
+      query = "CN=Default Query Policy,CN=Query-Policies,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,#{domain_dn}"
+      time = Facter::Core::Execution.execute("dsquery * \"#{query}\" -attr LDAPAdminLimits")
       time.match(%r{MaxConnIdleTime=(\d*)})[1]
     end
   end
