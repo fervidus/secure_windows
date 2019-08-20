302baba7e236b06c7800c0059314766b820ca572
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-08-20 22:55:42 +0200

    chomp variable first

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -5,8 +5,8 @@ Facter.add('maxconnidletime') do
   setcode do
     if Facter.value(:windows_server_type) == 'windowsdc'
       dcs = Facter::Core::Execution.execute('dsquery server')
-      dc_dn = dcs.lines.first.gsub!(/\A"|"\Z/, '').split(',')
-      domain_dn = dc_dn.last(2).join(',')
+      dc_dn = dcs.lines.first.gsub!('"', '').split(',')
+      domain_dn = dc_dn.last(2).join(',').chomp
       time = Facter::Core::Execution.execute("dsquery * \"CN=Default Query Policy,CN=Query-Policies,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,#{domain_dn}\" -attr LDAPAdminLimits")
       time.match(%r{MaxConnIdleTime=(\d*)})[1]
     end
