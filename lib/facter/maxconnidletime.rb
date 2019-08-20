74b662c61663d2e159d1641ea3aac6ef1806a8ba
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-08-20 21:38:34 +0200

    query real domain DN

--- lib/facter/maxconnidletime.rb
+++ lib/facter/maxconnidletime.rb
@@ -4,7 +4,10 @@ Facter.add('maxconnidletime') do
   confine operatingsystem: :windows
   setcode do
     if Facter.value(:windows_server_type) == 'windowsdc'
-      time = Facter::Core::Execution.execute('dsquery * "cn=Default Query Policy,cn=Query-Policies,cn=Directory Service,cn=Windows NT,cn=Services,cn=Configuration,dc=example,dc=com" -attr LDAPAdminLimits')
+      dcDNs = Facter::Core::Execution.execute('dsquery server')
+      dcDN = output.lines.first.gsub!(/\A"|"\Z/, '').split(",")
+      domainDN = dcDN.last(2).join(",")
+      time = Facter::Core::Execution.execute("dsquery * \"CN=Default Query Policy,CN=Query-Policies,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,#{domainDN}\" -attr LDAPAdminLimits")
       time.match(%r{MaxConnIdleTime=(\d*)})[1]
     end
   end
